f803a8efb7507b17fd88e439323973eff9101a78
Maikon Germano <mk_germano@hotmail.com>
2024-10-01 14:36:37 -0300

    feat: adição novos campos company

--- src/main/java/com/loja/auth/api_auth/model/entity/Company.java
+++ src/main/java/com/loja/auth/api_auth/model/entity/Company.java
@@ -1,7 +1,6 @@
 package com.loja.auth.api_auth.model.entity;
 
 import java.io.Serializable;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,17 +13,37 @@ import lombok.Data;
 @Table(name = "company")
 @Data
 public class Company implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
+    
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @Column(nullable = false)
     private String name;
 
     @Column
     private String address;
-
+    
+    @Column
+    private String clientDescription;
+    
+    @Column
+    private String titleHmtl;
+    
+    @Column
+    private String clientDomain;
+    
+    @Column
+    private String clientLogo;
+    
+    @Column
+    private String clientName;
+    
+    @Column
+    private Boolean useBackgroundDefaultPage;
+    
+    @Column
+    private String clientBackground;
 }
--- src/main/java/com/loja/auth/api_auth/security/DataInitialize.java
+++ src/main/java/com/loja/auth/api_auth/security/DataInitialize.java
@@ -35,6 +35,13 @@ public class DataInitialize implements CommandLineRunner {
             company = new Company();
             company.setName("Minha Empresa");
             company.setAddress("Rua Exemplo, 123, Cidade Exemplo"); // Definindo um endereço válido
+            company.setClientDescription("Levar a moda da rua para fora dela.");
+            company.setTitleHmtl("Jhon Roger");
+            company.setClientDomain("example.com"); // Substitua pelo domínio real
+            company.setClientLogo("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8GcwXkYZrbCU7lvGmBQyZHDu_KiwX-aKH5Q&s");
+            company.setClientName("Jhon Roger");
+            company.setUseBackgroundDefaultPage(false);
+            company.setClientBackground("https://scontent.fldb10-1.fna.fbcdn.net/v/t39.30808-6/439979087_7430909733661670_8943930251751380713_n.jpg?stp=dst-jpg_s960x960&_nc_cat=101&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=noa41nNvRxkQ7kNvgHtrtnQ&_nc_ht=scontent.fldb10-1.fna&_nc_gid=AAgLcoKb8kmOPhPb6b8fxl1&oh=00_AYAGk5JR3PQkVp6rzeNnAR8m40kdtiNqybFiTN34Wer4rA&oe=66EE8CEA");
 
             // Salvando a empresa no banco de dados
             company = companyRepository.save(company);
